ca93d14b65feb66251d54ce3fb1be9c84d3f0b31
chocolaggibbiddori <didrkd684@naver.com>
2025-01-21 23:30:39 +0900

    프로젝트 생성 도구 - Vue CLI, 싱글 파일 컴포넌트

---
+++ age-of-vue/vue-cli/src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <app-header v-bind:data="str" v-on:renew="renewStr"></app-header>
+  </div>
+</template>
+
+<script>
+import AppHeader from "@/components/AppHeader.vue";
+
+export default {
+  data() {
+    return {
+      str: 'Header'
+    }
+  },
+  components: {
+    AppHeader
+  },
+  methods: {
+    renewStr() {
+      this.str = 'Hi';
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ age-of-vue/vue-cli/src/components/AppHeader.vue
@@ -0,0 +1,21 @@
+<template>
+  <header>
+    <h1>{{ data }}</h1>
+    <button v-on:click="sendEvent">send</button>
+  </header>
+</template>
+
+<script>
+export default {
+  props: ['data'],
+  methods: {
+    sendEvent() {
+      this.$emit('renew');
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
