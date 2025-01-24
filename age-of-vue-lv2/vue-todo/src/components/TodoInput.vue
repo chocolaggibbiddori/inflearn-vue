d40d1da852b798e8084191ab720f3f36e0574e1e
chocolaggibbiddori <didrkd684@naver.com>
2025-01-24 21:56:39 +0900

    Todo App - 사용자 경험 개선

--- age-of-vue-lv2/vue-todo/src/components/TodoInput.vue
+++ age-of-vue-lv2/vue-todo/src/components/TodoInput.vue
@@ -2,19 +2,37 @@
   <div class="inputBox shadow">
     <input type="text" v-model="newTodoItem" v-on:keyup.enter="addTodo">
     <button v-on:click="addTodo">add</button>
+
+    <button id="show-modal" @click="showModal = true">Show Modal</button>
+    <modal v-if="showModal" @close="showModal = false">
+      <h3 slot="header">경고!</h3>
+      <div slot="body">무언가를 입력하세요.</div>
+      <footer slot="footer">copy right.</footer>
+    </modal>
   </div>
 </template>
 
 <script>
+import Modal from '@/common/Modal.vue';
+
 export default {
   name: "TodoInput",
+  components: {
+    Modal
+  },
   data() {
     return {
-      newTodoItem: ''
+      newTodoItem: '',
+      showModal: false
     }
   },
   methods: {
     addTodo() {
+      if (this.newTodoItem === '') {
+        this.showModal = true;
+        return;
+      }
+
       this.$emit('addTodo', this.newTodoItem);
       this.clearInput();
     },
--- age-of-vue-lv2/vue-todo/src/components/TodoList.vue
+++ age-of-vue-lv2/vue-todo/src/components/TodoList.vue
@@ -1,12 +1,12 @@
 <template>
   <div>
-    <ul>
+    <transition-group name="list" tag="ul">
       <li class="shadow" v-for="(todoItem, idx) in todoItems" v-bind:key="idx">
         <input type="checkbox" v-on:click="toggleComplete(idx)">
         <span v-bind:class="{ textCompleted: todoItem.completed }">{{ todoItem.item }}</span>
         <button v-on:click="removeTodo(todoItem, idx)">remove</button>
       </li>
-    </ul>
+    </transition-group>
   </div>
 </template>
 
@@ -28,5 +28,14 @@ export default {
 <style scoped>
 .textCompleted {
   text-decoration: line-through;
+}
+
+.list-enter-active, .list-leave-active {
+  transition: all 1s;
+}
+
+.list-enter, .list-leave-to {
+  opacity: 0;
+  transform: translateY(30px);
 }
 </style>
