d2ff5d2ff5c072eccfd771e5e2c516961b0efc4c
chocolaggibbiddori <didrkd684@naver.com>
2025-01-30 00:16:08 +0900

    Store 구현 - NewsView에 actions와 mutations 적용

--- age-of-vue-lv3/vue-news/src/store/index.js
+++ age-of-vue-lv3/vue-news/src/store/index.js
@@ -1,10 +1,23 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
+import { fetchNewsList } from '@/api';
 
 Vue.use(Vuex);
 
 export default new Vuex.Store({
   state: {
     newsList: []
+  },
+  mutations: {
+    setNewsList(state, payload) {
+      state.newsList = payload;
+    }
+  },
+  actions: {
+    fetchNewsList({ commit }) {
+      fetchNewsList()
+        .then(res => commit('setNewsList', res.data))
+        .catch(error => console.error(error));
+    }
   }
 });
--- age-of-vue-lv3/vue-news/src/views/NewsView.vue
+++ age-of-vue-lv3/vue-news/src/views/NewsView.vue
@@ -1,22 +1,15 @@
 <template>
   <div>
-    <div v-for="news in newsList" :key="news.id">{{ news.title }}</div>
+    <div v-for="news in this.$store.state.newsList" :key="news.id">
+      {{ news.title }}
+    </div>
   </div>
 </template>
 
 <script>
-import { fetchNewsList } from '@/api';
-
 export default {
-  data() {
-    return {
-      newsList: []
-    };
-  },
   created() {
-    fetchNewsList()
-      .then(res => (this.newsList = res.data))
-      .catch(error => console.error(error));
+    this.$store.dispatch('fetchNewsList');
   }
 };
 </script>
