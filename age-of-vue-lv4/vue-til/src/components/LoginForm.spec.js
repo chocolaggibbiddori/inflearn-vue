12794135473c09da6e73d97507ecf50594896122
chocolaggibbiddori <didrkd684@naver.com>
2025-02-12 23:17:11 +0900

    테스트 - username 테스트

--- age-of-vue-lv4/vue-til/src/components/LoginForm.spec.js
+++ age-of-vue-lv4/vue-til/src/components/LoginForm.spec.js
@@ -5,12 +5,24 @@ import { shallowMount } from '@vue/test-utils';
 describe('LoginForm.vue', () => {
   test('컴포넌트가 마운팅 되면 화면에 그려져야 한다.', () => {
     const instance = new Vue(LoginForm).$mount();
-    console.log(instance);
     expect(instance.username).toBe('');
   });
 
   test('컴포넌트가 마운딩되면 username이 존재하고 초기값으로 설정되어 있어야 한다.', () => {
     const wrapper = shallowMount(LoginForm);
     expect(wrapper.vm.username).toBe('');
+  });
+
+  test('ID는 이메일 형식이어야 한다.', () => {
+    const wrapper = shallowMount(LoginForm, {
+      data() {
+        return {
+          username: 'test@abc.com'
+        };
+      }
+    });
+    const username = wrapper.find('#username');
+    console.log(username.element.value);
+    console.log(wrapper.vm.isUsernameValid);
   });
 });
