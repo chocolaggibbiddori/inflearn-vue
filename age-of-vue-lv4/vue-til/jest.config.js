ac436979dbea98ff1114ecf1068e3da6891bc9c9
chocolaggibbiddori <didrkd684@naver.com>
2025-02-12 23:01:55 +0900

    테스트 - 컴포넌트 테스트

--- age-of-vue-lv4/vue-til/jest.config.js
+++ age-of-vue-lv4/vue-til/jest.config.js
@@ -2,5 +2,6 @@ module.exports = {
   preset: '@vue/cli-plugin-unit-jest',
   testMatch: [
     '<rootDir>/src/**/*.spec.(js|jsx|ts|tsx)|**/__tests__/*.(js|jsx|ts|tsx)'
-  ]
+  ],
+  transformIgnorePatterns: ['node_modules/(?!axios)']
 };
--- age-of-vue-lv4/vue-til/src/components/LoginForm.spec.js
+++ age-of-vue-lv4/vue-til/src/components/LoginForm.spec.js
@@ -1,8 +1,15 @@
-import { sum } from './math';
+import Vue from 'vue';
+import LoginForm from './LoginForm';
 
-describe('math.js', () => {
-  test('sum 10 and 20', () => {
-    const result = sum(10, 20);
-    expect(result).not.toBe(20);
+describe('LoginForm.vue', () => {
+  test('컴포넌트가 마운팅 되면 화면에 그려져야 한다.', () => {
+    const instance = new Vue(LoginForm).$mount();
+    console.log(instance);
+    expect(instance.username).toBe('');
+  });
+
+  test('컴포넌트가 마운딩되면 username이 존재하고 초기값으로 설정되어 있어야 한다.', () => {
+    const instance = new Vue(LoginForm).$mount();
+    expect(instance.username).toBe('');
   });
 });
