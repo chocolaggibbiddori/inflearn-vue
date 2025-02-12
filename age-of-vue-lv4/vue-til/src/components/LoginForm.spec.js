8fa7b099208a763c7c96c977cf7a60e4ed6a09da
chocolaggibbiddori <didrkd684@naver.com>
2025-02-12 23:05:37 +0900

    테스트 - 뷰 테스트 유틸 라이브러리

--- age-of-vue-lv4/vue-til/src/components/LoginForm.spec.js
+++ age-of-vue-lv4/vue-til/src/components/LoginForm.spec.js
@@ -1,5 +1,6 @@
 import Vue from 'vue';
 import LoginForm from './LoginForm';
+import { shallowMount } from '@vue/test-utils';
 
 describe('LoginForm.vue', () => {
   test('컴포넌트가 마운팅 되면 화면에 그려져야 한다.', () => {
@@ -9,7 +10,7 @@ describe('LoginForm.vue', () => {
   });
 
   test('컴포넌트가 마운딩되면 username이 존재하고 초기값으로 설정되어 있어야 한다.', () => {
-    const instance = new Vue(LoginForm).$mount();
-    expect(instance.username).toBe('');
+    const wrapper = shallowMount(LoginForm);
+    expect(wrapper.vm.username).toBe('');
   });
 });
